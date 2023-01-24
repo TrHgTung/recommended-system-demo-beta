a089563bbe33a61fc138da1a1b9921fcc9f7aba0
tung <tungng14@gmail.com>
2023-01-24 23:54:54 +0700

    add matrix

---
+++ user_recommendation.php
@@ -0,0 +1,16 @@
+<?php
+    include("db.php");
+    $movies = mysqli_query($con, "select * from user_movies");
+    $matrix = array();
+
+    while($movie = mysqli_fetch_array($movies)){
+        $users = mysqli_query($con, "select username from users where id=$movie[user_id]");
+        $username = mysqli_fetch_array($users);
+
+        $matrix[$username['username']][$movie['movie_name']] = $movie['movie_rating'];
+    }
+    echo "<pre>";
+        print_r($matrix);
+    echo "</pre>";
+
+?>
